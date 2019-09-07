801b12c5c9703144a248793bfbd3abb1bf8a072b
Farhad Alemi <farhad.alemi@berkeley.edu>
2019-09-07 13:42:27 -0700

    lab00

---
+++ lab00/lab00.py
@@ -0,0 +1,8 @@
+def twenty_nineteen():
+    """Come up with the most creative expression that evaluates to 2019,
+    using only numbers and the +, *, and - operators.
+
+    >>> twenty_nineteen()
+    2019
+    """
+    return + 0 - 1 + 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 - 3 + 4 - 5 + 6 - 7 + 8 - 9 + 8 - 7 + 6 - 5 + 4 * 3 * 2 * 1 * -1
