80b30c7316d37958c16012f72332f255268b08a1
Farhad Alemi <farhad.alemi@berkeley.edu>
2019-09-11 13:47:03 -0700

    lab01

---
+++ lab01/lab01_extra.py
@@ -0,0 +1,46 @@
+"""Optional questions for Lab 1"""
+
+# While Loops
+
+def falling(n, k):
+    """Compute the falling factorial of n to depth k.
+
+    >>> falling(6, 3)  # 6 * 5 * 4
+    120
+    >>> falling(4, 3)  # 4 * 3 * 2
+    24
+    >>> falling(4, 1)  # 4
+    4
+    >>> falling(4, 0)
+    1
+    """
+    product = 1
+    iterator = 0
+    while (iterator < k):
+        product *= (n - iterator)
+        iterator += 1
+
+    return product
+
+def double_eights(n):
+    """Return true if n has two eights in a row.
+    >>> double_eights(8)
+    False
+    >>> double_eights(88)
+    True
+    >>> double_eights(2882)
+    True
+    >>> double_eights(880088)
+    True
+    >>> double_eights(12345)
+    False
+    >>> double_eights(80808080)
+    False
+    """
+    while (n != 0):
+        if (n % 10 == 8 and ((n // 10) % 10) == 8):
+            return True
+        else:
+            n = n // 10
+
+    return False
